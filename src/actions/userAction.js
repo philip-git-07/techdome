61068329f44710fa23032317a1be86a62f7c86ea
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2023-12-10 09:22:23 +0000

    Update userAction.js

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -2,59 +2,59 @@ import { LOGIN_SUCCESS, LOAD_SUCCESS, LOGOUT_SUCCESS } from "../constants/userCo
 import axios from "../apiConfig/api";
 
 export const registerUser = (userData, navigate, fun) => async () => {
-    try {
-        const config = { headers: { "Content-Type": "application/json" } };
-        const res = await axios.post("/register", userData, config);
-        if (res.status === 201) {
-            navigate("/login");
-        } else {
-            window.alert("Something went wrong");
-        }
-        fun(null);
-    } catch (error) {
-        fun(error.response.data.message);
-        console.error(error);
+  try {
+    const config = { headers: { "Content-Type": "application/json" } };
+    const res = await axios.post("/register", userData, config);
+    if (res.status === 201) {
+      navigate("/login");
+    } else {
+      window.alert("Something went wrong");
     }
+    fun(null);
+  } catch (error) {
+    fun(error.response.data.message);
+    console.error(error);
+  }
 };
 
 export const loginUser = (userData, fun) => async (dispatch) => {
-    try {
-        const config = { headers: { "Content-Type": "application/json" } };
-        const res = await axios.post("/login", userData, config);
-        if (res.status === 200) {
-            localStorage.setItem("userToken", res.data.token);
-            dispatch({ type: LOGIN_SUCCESS, payload: res.data.user });
-        } else {
-            window.alert("Something went wrong");
-        }
-        fun(null);
-    } catch (error) {
-        fun(error.response.data.message);
-        console.error(error);
+  try {
+    const config = { headers: { "Content-Type": "application/json" } };
+    const res = await axios.post("/login", userData, config);
+    if (res.status === 200) {
+      localStorage.setItem("userToken", res.data.token);
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data.user });
+    } else {
+      window.alert("Something went wrong");
     }
+    fun(null);
+  } catch (error) {
+    fun(error.response.data.message);
+    console.error(error);
+  }
 };
 
 export const loadUser = () => async (dispatch) => {
-    try {
-        const token = localStorage.getItem("userToken");
-        if (token) {
-            const config = { headers: { "Content-Type": "application/json" } };
-            const res = await axios.post("/get_user_data", { token }, config);
-            dispatch({ type: LOAD_SUCCESS, payload: res.data.user });
-        } else {
-            console.log("Token is not present!");
-        }
-    } catch (error) {
-        console.error(error);
+  try {
+    const token = localStorage.getItem("userToken");
+    if (token) {
+      const config = { headers: { "Content-Type": "application/json" } };
+      const res = await axios.post("/get_user_data", { token }, config);
+      dispatch({ type: LOAD_SUCCESS, payload: res.data.user });
+    } else {
+      console.log("Token is not present!");
     }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const logoutUser = (navigate) => async (dispatch) => {
-    try {
-        localStorage.removeItem("userToken");
-        dispatch({ type: LOGOUT_SUCCESS });
-        navigate("/login");
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    localStorage.removeItem("userToken");
+    dispatch({ type: LOGOUT_SUCCESS });
+    navigate("/login");
+  } catch (error) {
+    console.error(error);
+  }
 };
