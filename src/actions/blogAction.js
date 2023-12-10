633f1313eb3df4e3f90d7771acd03bc3735d41d3
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2023-12-10 08:38:34 +0000

    Update blogAction.js

--- src/actions/blogAction.js
+++ src/actions/blogAction.js
@@ -3,82 +3,76 @@ import axios from "axios";
 import axiosNew from "../apiConfig/api";
 
 const uploadData = async (data, dispatch) => {
-    try {
-        const config = { headers: { "Content-Type": "application/json" } };
-        const res = await axiosNew.post("/add_blog", data, config);
-        dispatch({ type: ADD_NEW_BLOG, payload: res.data.blog });
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const config = { headers: { "Content-Type": "application/json" } };
+    const res = await axiosNew.post("/add_blog", data, config);
+    dispatch({ type: ADD_NEW_BLOG, payload: res.data.blog });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const updateData = async (data, dispatch) => {
-    try {
-        const config = { headers: { "Content-Type": "application/json" } };
-        const res = await axiosNew.post("/update_blog", data, config);
-        dispatch({ type: UPDATE_NEW_BLOG, payload: res.data.newBlog });
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const config = { headers: { "Content-Type": "application/json" } };
+    const res = await axiosNew.post("/update_blog", data, config);
+    dispatch({ type: UPDATE_NEW_BLOG, payload: res.data.newBlog });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const addBlog = (blogData) => async (dispatch) => {
-    try {
-        dispatch({ type: BLOG_REQUEST });
-        const data = new FormData();
-        data.append("file", blogData.img);
-        data.append("upload_preset", "myappintern");
-        axios.post("https://api.cloudinary.com/v1_1/dplwvxqum/image/upload", data)
-            .then(({ data }) => {
-                uploadData({ ...blogData, img: data.url, public_id: data.public_id }, dispatch);
-            })
-            .catch((err) => console.log(err));
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    dispatch({ type: BLOG_REQUEST });
+    const data = new FormData();
+    data.append("file", blogData.img);
+    data.append("upload_preset", "myappintern");
+    const { data: imageData } = await axios.post("https://api.cloudinary.com/v1_1/dplwvxqum/image/upload", data);
+    uploadData({ ...blogData, img: imageData.url, public_id: imageData.public_id }, dispatch);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const deleteBlog = (blogData) => async (dispatch) => {
-    try {
-        dispatch({ type: DEL_BLOG, payload: blogData._id });
-        const config = { headers: { "Content-Type": "application/json" } };
-        const { data } = await axiosNew.post("/delete_blog", blogData, config);
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    dispatch({ type: DEL_BLOG, payload: blogData._id });
+    const config = { headers: { "Content-Type": "application/json" } };
+    const { data } = await axiosNew.post("/delete_blog", blogData, config);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const updateBlog = (blogData) => async (dispatch) => {
-    try {
-        dispatch({ type: BLOG_REQUEST });
-        const config = { headers: { "Content-Type": "application/json" } };
+  try {
+    dispatch({ type: BLOG_REQUEST });
+    const config = { headers: { "Content-Type": "application/json" } };
 
-        if (blogData.imgChange) {
-            const del_id = blogData.public_id;
-            const data = new FormData();
-            data.append("file", blogData.img);
-            data.append("upload_preset", "myappintern");
-            axios.post("https://api.cloudinary.com/v1_1/dplwvxqum/image/upload", data)
-                .then(({ data }) => {
-                    updateData({ ...blogData, img: data.url, public_id: data.public_id, del_id }, dispatch);
-                })
-                .catch((err) => console.log(err));
-        } else {
-            const { data } = await axiosNew.post("/update_blog", blogData, config);
-            dispatch({ type: UPDATE_NEW_BLOG, payload: data.newBlog });
-        }
-    } catch (error) {
-        console.log(error);
+    if (blogData.imgChange) {
+      const del_id = blogData.public_id;
+      const data = new FormData();
+      data.append("file", blogData.img);
+      data.append("upload_preset", "myappintern");
+      const { data: imageData } = await axios.post("https://api.cloudinary.com/v1_1/dplwvxqum/image/upload", data);
+      updateData({ ...blogData, img: imageData.url, public_id: imageData.public_id, del_id }, dispatch);
+    } else {
+      const { data } = await axiosNew.post("/update_blog", blogData, config);
+      dispatch({ type: UPDATE_NEW_BLOG, payload: data.newBlog });
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getBlogs = () => async (dispatch) => {
-    try {
-        dispatch({ type: BLOG_REQUEST });
-        const { data } = await axiosNew.get("/get_blog");
-        dispatch({ type: BLOG_SUCCESS, payload: data.blogs });
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    dispatch({ type: BLOG_REQUEST });
+    const { data } = await axiosNew.get("/get_blog");
+    dispatch({ type: BLOG_SUCCESS, payload: data.blogs });
+  } catch (error) {
+    console.log(error);
+  }
+};
