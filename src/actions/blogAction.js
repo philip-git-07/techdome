9deccc8d6dfac890e894a342766e9299e14ee482
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2023-12-10 10:05:53 +0000

    Update blogAction.js

--- src/actions/blogAction.js
+++ src/actions/blogAction.js
@@ -1,8 +1,6 @@
 import { ADD_BLOG, BLOG_REQUEST, BLOG_SUCCESS, ADD_NEW_BLOG, DEL_BLOG, UPDATE_NEW_BLOG } from "../constants/blogConstants";
 import axios from "axios";
 import axiosNew from "../apiConfig/api";
-import { registerUser } from  '../../actions/userAction' ;
-
 
 
 
