26a385ce6fca05f84fdf279448ccb2e6990cf2bd
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2023-12-10 08:24:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/login/Login'
-import Signup from './pages/signup/Signup'
-import Blog from './pages/blog/Blog'
-import Form from './pages/form/Form'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { loadUser } from './actions/userAction'
+import React, { useEffect } from 'react'; // Importing useEffect from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Blog from './pages/blog/Blog';
+import Form from './pages/form/Form';
+import { useDispatch } from 'react-redux';
+import { loadUser } from './actions/userAction';
 
 const App = () => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadUser());
-  }, []);
+  }, [dispatch]); // Add 'dispatch' as a dependency here
 
   return (
     <>
@@ -25,7 +23,7 @@ const App = () => {
         <Route path='/signup' element={<Signup />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
